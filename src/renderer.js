8c2ebf5d7227ffe6a5661863dedc186e83d67767
AshleyAst <164010909+AshleyAst@users.noreply.github.com>
2024-08-30 19:40:21 +0700

    mass removal of nearly all win9 files

--- src/renderer.js
+++
@@ -1,1 +0,0 @@
-window.versions.back()
--- src/windows/renderer.js
+++
@@ -1,9 +0,0 @@
-const ipc = require('electron').ipcRenderer
-
-// close app
-function closeApp(e) {
-  e.preventDefault()
-  ipc.send('close')
-}
-
-document.getElementById('shutdown').addEventListener('ended',closeApp,false);
