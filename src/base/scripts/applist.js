cd407be4da7e88fc3a0b80aef98c666484561b3d
AshleyAst <164010909+AshleyAst@users.noreply.github.com>
2024-08-30 19:41:57 +0700

    Added a new base template by https://github.com/notAperson535/Win11-HTML

---
+++ src/base/scripts/applist.js
@@ -0,0 +1,13 @@
+// Syntax = createapp("app name (lowercase no spaces)", "url", "icon (can be png or other)", "visual app name (friendly name)")
+
+createpremadeappwithtaskbaricon("explorer", "", "img/explorer.png", "File Exporer");
+
+createapp("word", "./apps/word.html", "img/word.ico", "Word");
+createapp("excel", "./apps/excel.html", "img/excel.ico", "Excel");
+createapp("powerpoint", "https://app.deckdeckgo.com", "img/powerpoint.ico", "PowerPoint");
+createpremadeapp("notepad", "", "img/notepad.ico", "Notepad");
+createapp("calculator", "https://calculator.notaperson535.is-a.dev", "img/calculator.ico", "Calculator");
+createpremadeappwithtaskbaricon("store", "", "img/store.ico", "Store");
+createapp("vscode", "https://vscode.dev", "img/vscode.ico", "VS Code");
+createapp("solitaire", "https://player.zone.msn.com/?hostingEnvironment=EdgeCasualGamesHub&gamepath=mssolitairecollection", "img/solitaire.ico", "Solitaire");
+createapp("whiteboard", "./apps/whiteboard/index.html", "img/whiteboard.png", "Whiteboard")
